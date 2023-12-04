d15821a9e08e6e153833d1747cd28b041d51650c
silasramosg <silocasfframos@gmail.com>
2023-12-04 15:06:52 -0300

    ultimo commit

---
+++ assets/js/detalhes.js
@@ -0,0 +1,14 @@
+var produtoImg = document.getElementById("produtoImg");
+var produtoMiniatura = document.getElementsByClassName("produtoMiniatura");
+
+produtoMiniatura[0].onclick = function(){
+    produtoImg.src = produtoMiniatura[0].src;
+}
+
+produtoMiniatura[1].onclick = function(){
+    produtoImg.src = produtoMiniatura[1].src;
+}
+
+produtoMiniatura[2].onclick = function(){
+    produtoImg.src = produtoMiniatura[2].src;
+}
